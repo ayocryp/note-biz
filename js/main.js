1cc132ac25cf9b784802b1ca9a456919d175c12e
your_username <ayodejifasesin4505@gmail.com>
2022-06-03 10:49:40 -0500

    js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
+
+
+// document.querySelector('.order-payment').addEventListener('click', makePayment())
+
+function makePayment() {
+  FlutterwaveCheckout({
+    public_key: "FLWPUBK_TEST-SANDBOXDEMOKEY-X",
+    tx_ref: "titanic-48981487343MDI0NzMx",
+    amount: 54600,
+    currency: "NGN",
+    payment_options: "card, mobilemoneyghana, ussd",
+    redirect_url: "https://mummyserena.netlify.app",
+    meta: {
+      consumer_id: 23,
+      consumer_mac: "92a3-912ba-1192a",
+    },
+    customer: {
+      email: "rose@unsinkableship.com",
+      phone_number: "08102909304",
+      name: "Rose DeWitt Bukater",
+    },
+    customizations: {
+      title: "The Titanic Store",
+      description: "Payment for an awesome cruise",
+      logo: "https://www.logolynx.com/images/logolynx/22/2239ca38f5505fbfce7e55bbc0604386.jpeg",
+    },
+  });
+}
+
 window.onload = function () {
   Particles.init({
       selector: ".background",
@@ -5,4 +34,3 @@ window.onload = function () {
       color: 'black'
   });
 };
-
