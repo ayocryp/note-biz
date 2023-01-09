c4e934c7b60eb7a887250ba6b0cf5079fd609bcc
your_username <ayodejifasesin4505@gmail.com>
2023-01-09 06:52:57 -0600

    added consent form

--- js/main.js
+++ js/main.js
@@ -1,28 +1,3 @@
-// Flutterwave API consumption
-function makePayment() {
-  FlutterwaveCheckout({
-    public_key: "FLWPUBK_TEST-SANDBOXDEMOKEY-X",
-    tx_ref: "titanic-48981487343MDI0NzMx",
-    amount: 54600,
-    currency: "NGN",
-    payment_options: "card, mobilemoneyghana, ussd",
-    redirect_url: "https://mummyserena.netlify.app",
-    meta: {
-      consumer_id: 23,
-      consumer_mac: "92a3-912ba-1192a",
-    },
-    customer: {
-      email: "rose@unsinkableship.com",
-      phone_number: "08102909304",
-      name: "Rose DeWitt Bukater",
-    },
-    customizations: {
-      title: "The Titanic Store",
-      description: "Payment for an awesome cruise",
-      logo: "https://www.logolynx.com/images/logolynx/22/2239ca38f5505fbfce7e55bbc0604386.jpeg",
-    },
-  });
-}
 
 
 // Background particles
@@ -58,7 +33,6 @@ function PreventSubmission () {
         event.preventDefault()
         event.stopPropagation()
         makePayment()
-
       }
 
       form.classList.add('was-validated')
@@ -68,8 +42,28 @@ function PreventSubmission () {
 
 PreventSubmission()
 
-// const buttn = document.querySelector('.order-payment')
-
-// if (PreventSubmission !== true) {
-//   buttn
-// }
+// Flutterwave API consumption
+function makePayment() {
+  FlutterwaveCheckout({
+    public_key: "FLWPUBK_TEST-SANDBOXDEMOKEY-X",
+    tx_ref: "titanic-48981487343MDI0NzMx",
+    amount: 54600,
+    currency: "NGN",
+    payment_options: "card, mobilemoneyghana, ussd",
+    redirect_url: "https://mummyserena.netlify.app",
+    meta: {
+      consumer_id: 23,
+      consumer_mac: "92a3-912ba-1192a",
+    },
+    customer: {
+      email: "rose@unsinkableship.com",
+      phone_number: "08102909304",
+      name: "Rose DeWitt Bukater",
+    },
+    customizations: {
+      title: "The Titanic Store",
+      description: "Payment for an awesome cruise",
+      logo: "https://www.logolynx.com/images/logolynx/22/2239ca38f5505fbfce7e55bbc0604386.jpeg",
+    },
+  });
+}
