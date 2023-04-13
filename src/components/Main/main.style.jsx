f4db5c89a4b8f32d50eea0b621b828366f9e2f25
your_username <ayodejifasesin4505@gmail.com>
2023-04-13 00:30:30 -0500

    changed the font family for article text

--- src/components/Main/main.style.jsx
+++ src/components/Main/main.style.jsx
@@ -25,15 +25,16 @@ export const ArticleTitle = styled.div`
   color: #fb9129;
   font-family: "Millik", sans-serif !important;
   margin: 0;
-  font-size: 70px;
-  font-weight: 500;
+  font-size: 90px;
+  font-weight: 600;
   @media (max-width: 900px) {
     font-size: 35px;
   }
 
   span {
     color: black;
-    font-size: 50px;
+    font-size: 54px;
+    font-weight: 400;
     font-family: "Millik", sans-serif !important;
   }
 `;
@@ -43,7 +44,9 @@ export const ArticleText = styled.div`
   p {
     margin: 0;
     color: #212529;
-    line-height: 1.7rem;
+    line-height: 1.9rem;
+    font-family: "Moderat", sans-serif !important;
+    font-weight: 100;
   }
 `;
 
@@ -71,11 +74,14 @@ export const OrderBtn = styled.div`
   width: fit-content;
   background-color: #0a0e27;
   cursor: pointer;
-  padding: 12px 16px;
-  font-size: 1.25rem;
-  border-radius: 10px;
+  padding: 15px 16px;
+  font-size: 1rem;
+  font-family: "Moderat", sans-serif !important;
+  font-weight: 100;
+  border-radius: 10px 10px;
   :hover {
-    box-shadow: 0 5px 10px rgba(29, 29, 29, 0.5);
+    // box-shadow: 0 5px 10px rgba(29, 29, 29, 0.5);
+    // background: #fb9129;
   }
 `;
 export const MainWrapper = styled.div`
