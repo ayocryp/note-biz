82447ebe39bac7f93f5736b43244aa478b1ffbc5
redblood639 <albinorabbithunter@gmail.com>
2023-06-10 10:46:29 -0400

    fix

--- src/components/FAQ/index.jsx
+++ src/components/FAQ/index.jsx
@@ -2,40 +2,33 @@ import { FaqContainer, FaqWrapper, SectionTitle } from "./faq.style";
 import Accordion from "react-bootstrap/Accordion";
 
 const FAQ_DATA = [
-  {
-    question: "Why GCMS notes?",
-    answer:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus, magnam.",
-  },
-  {
-    question: "Why GCMS?",
-    answer:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus, magnam.",
-  },
+	{
+		question: "Why GCMS notes?",
+		answer:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus, magnam.",
+	},
+	{
+		question: "Why GCMS?",
+		answer:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus, magnam.",
+	},
 ];
 
 const FaqComponent = () => (
-  <FaqContainer id="faq">
-    <FaqWrapper>
-      <SectionTitle>Frequently Asked Questions</SectionTitle>
-      <Accordion flush data-aos="fade-up">
-        {FAQ_DATA.map(({ question, answer, index }) => (
-          <Accordion.Item eventKey={index} key={question}>
-            <Accordion.Header>{question}</Accordion.Header>
-            <Accordion.Body>{answer}</Accordion.Body>
-            <Accordion.Header>{question}</Accordion.Header>
-            <Accordion.Body>{answer}</Accordion.Body>
-            <Accordion.Header>{question}</Accordion.Header>
-            <Accordion.Body>{answer}</Accordion.Body>
-            <Accordion.Header>{question}</Accordion.Header>
-            <Accordion.Body>{answer}</Accordion.Body>
-            <Accordion.Header>{question}</Accordion.Header>
-            <Accordion.Body>{answer}</Accordion.Body>
-          </Accordion.Item>
-        ))}
-      </Accordion>
-    </FaqWrapper>
-  </FaqContainer>
+	<FaqContainer id="faq">
+		<FaqWrapper>
+			<SectionTitle>Frequently Asked Questions</SectionTitle>
+
+			<Accordion defaultActiveKey="0">
+				{FAQ_DATA.map((item, index) => (
+					<Accordion.Item eventKey={"key" + index}>
+						<Accordion.Header>{item.question}</Accordion.Header>
+						<Accordion.Body>{item.answer}</Accordion.Body>
+					</Accordion.Item>
+				))}
+			</Accordion>
+		</FaqWrapper>
+	</FaqContainer>
 );
 
 export default FaqComponent;
